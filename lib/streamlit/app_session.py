bd4a22d23cf3a93f306b4a3f62bd5da66763fd27
Maya Barnes <63436329+mayagbarnes@users.noreply.github.com>
2022-01-18 14:13:39 -0800

    Fix unintended import/lint errors

--- lib/streamlit/app_session.py
+++ lib/streamlit/app_session.py
@@ -256,12 +256,10 @@ class AppSession:
                 ),
                 None,
             )
-            script_path = page_desc['script_path']
+            script_path = page_desc["script_path"]
 
             if script_path:
-                LOGGER.debug(
-                    f"received rerun request for script at path {script_path}"
-                )
+                LOGGER.debug(f"received rerun request for script at path {script_path}")
 
             rerun_data = RerunData(
                 client_state.query_string,
